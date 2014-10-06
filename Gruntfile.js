2c5e44c7071b587f64e2ff983b15e57db18de19a
Pony <pwsmith@gracenote.com>
2014-10-05 23:47:26 -0700

    Adding grunt tasks for compiling CSS and JS

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),      
+        uglify: {
+            my_target: {
+                files: {
+                    'js/tabletrim.min.js': ['js/tabletrim.js']
+                }
+            }
+        },
+        cssmin: {
+            add_banner: {
+                options: {
+                    banner: '/* tabletrim.js | https://github.com/ponysmith/tabletrim */'
+                },
+                files: {
+                    'css/tabletrim.min.css': ['css/**/*.css']
+                }
+            }
+        }
+    });
+
+
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    grunt.registerTask('default', ['uglify', 'cssmin']);
+
+}
